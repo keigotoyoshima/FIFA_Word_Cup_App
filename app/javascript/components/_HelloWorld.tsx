61b14d913571f7570f29c945e778cd1878a2d6cb
keigotoyo <keigo.work199987s@gmail.com>
2022-07-12 13:03:25 +0900

    react使えるようにした

---
+++ app/javascript/components/_HelloWorld.tsx
@@ -0,0 +1,14 @@
+// import React from "react"
+
+// // type Props = {
+// //   greeting: string
+// // }
+
+// const HelloWorld : React.FC = () => {
+//     return (
+//       <div className="App">
+//         <p>ハロー React!!</p>
+//       </div>
+//     )
+// }
+// export default HelloWorld
