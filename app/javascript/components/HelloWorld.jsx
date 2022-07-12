750b26edd921e17b923fd7720410dc00bbe0857e
keigotoyo <keigo.work199987s@gmail.com>
2022-07-12 14:32:05 +0900

    ひとまずフロントをreactで実装

---
+++ app/javascript/components/HelloWorld.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+
+const HelloWorld = ({greeting}) => {
+
+  return (
+    <div>
+      <h3>this is react component</h3>
+      <h4>greeting</h4>
+    </div>
+  );
+};
+
+
+export default HelloWorld
+
+
