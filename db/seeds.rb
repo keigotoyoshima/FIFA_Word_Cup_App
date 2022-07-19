6c7fcc28dbd5bbbaaaa821b411e1b0c94aff4e7e
keigotoyo <keigo.work199987s@gmail.com>
2022-07-19 11:57:58 +0900

    seeds.rbにテストデータ書いてrails db:seedすると登録される

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# coding: utf-8
+
+Country.create(name: 'Spain')
+Country.create(name: 'German') 
