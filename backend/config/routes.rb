60b01a3387dfb543ce7b5650d65c25c695e1f7b3
keigotoyo <keigo.work199987s@gmail.com>
2022-09-04 18:36:06 +0900

    DBに投入したデータを JSON 形式で取得

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get '/', to:'home_page#index'
-  get 'sample/index', to: 'sample#index'
-  get 'group_page', to: 'group_page#index'
+  resources :posts, only: :index
+  # get '/', to:'home_page#index'
+  # get 'sample/index', to: 'sample#index'
+  # get 'group_page', to: 'group_page#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
